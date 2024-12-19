2d42d5ef5f2e5682490b1ae8f493cff8dad0934e
Jatin Aggarwal <jaggarwal147@gmail.com>
2024-12-19 15:20:14 -0800

    backend call for deafult value

--- src/atoms.js
+++ src/atoms.js
@@ -1,10 +1,19 @@
 import { atom, selector } from "recoil";
 
+
+//-------------------for getting default values from backend-----------
+
 export const networkAtom = atom({
     key: "networkAtom",
-    default: 1
+    default: selector({
+        key:"networkAtomSelector",
+        get: async ()=>{
+            const res = await axios.get("https://sum-server.1000.com/notification")
+            return res.data
+        }
+    })
 })
-
+//------------------------------
 export const jobsAtom = atom({
     key:"jobsAtom",
     default: 2
@@ -33,3 +42,23 @@ export const totalNotifcationselector = selector({
        
     }
 })
+
+// ---------------------------------------------
+//     export const totalCount = atom({
+//       key:"totalCount",
+//     default :{
+//     network : 1
+//     jobs : 2
+//     notification :3
+//     messages :4  
+//     }})
+
+
+// export const totacount = selector({
+//     key:"totalcount",
+//     get:({get})=>{
+//         const count = get(totalCount)
+
+//         return count.messages + count.jobs + count.network + count.notification
+//     }
+// })
